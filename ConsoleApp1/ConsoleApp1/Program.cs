3e6331d82162b585dd44a4d3e3bfeb4ca6aa081f
DrDomi2 <dvidovic1@etfos.hr>
2023-11-03 17:10:08 +0100

    Dodavanje konzolne aplikacije

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Pozdrav iz aplikacije od Valentina Jelavic (0123456789)");
+            Console.WriteLine("Pozdrav iz aplikacije od Valentina Jelavic");
+
         }
     }
 }
